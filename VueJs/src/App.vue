3104e00ae85dcaa7178d6615683b5ac94e08fb83
Yanis <yanisariouapro@gmail.com>
2020-11-20 11:51:47 +0100

    ajout du css et de counter.vue

---
+++ VueJs/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{msg}}</h1>
+    <counter></counter>
+    <counter></counter>
+    <counter></counter>
+    <counter></counter>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue'
+
+export default {
+  components: {Counter},
+  data() {
+    return {
+      msg: 'Hello Vue'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
